aaf567cb93295b15864f0d86b32bfa215dfa4ab1
Eniko Csanadi <csanadieniko@gmail.com>
2019-03-25 07:37:15 +0100

    Send event insted of positions.

--- rotary_drawing.ino
+++ rotary_drawing.ino
@@ -2,31 +2,39 @@
 
 */
 
+//TODO: set as HID if Arduino Leonardo, Micro or Pro Micro (or Due, Zero, M0). Those can emulate a keyboard.
+
 // Variables for left control
-int encoderLPinA = 5;
+int encoderLPinA = 7;
 int encoderLPinB = 6;
-int encoderLClk = 7;
+int encoderLSw = 5;
 int encoderLPos = 0;
 int encoderLPinALast = LOW;
 int l = LOW;
 
 //Variables for right control
-int encoderRPinA = 2;
+int encoderRPinA = 4;
 int encoderRPinB = 3;
-int encoderRClk = 4;
+int encoderRSw = 2;
 int encoderRPos = 0;
 int encoderRPinALast = LOW;
 int r = LOW;
+
 int lButtonState = LOW;
 int rButtonState = LOW;
+int lButtonState_last = LOW;
+int rButtonState_last = LOW;
+
+int counterl =0;
+int counterr =0;
 
 void setup() {
   pinMode (encoderLPinA, INPUT);
   pinMode (encoderLPinB, INPUT);
-  pinMode (encoderLClk, INPUT);
+  pinMode (encoderLSw, INPUT_PULLUP);
   pinMode (encoderRPinA, INPUT);
   pinMode (encoderRPinB, INPUT);
-  pinMode (encoderRClk, INPUT);
+  pinMode (encoderRSw, INPUT_PULLUP);
   Serial.begin (9600);
 }
 
@@ -36,33 +44,56 @@ void loop() {
   r = digitalRead(encoderRPinA);
   if ((encoderLPinALast == LOW) && (l == HIGH)) {
     if (digitalRead(encoderLPinB) == LOW) {
-      encoderLPos--;
+      //encoderLPos--;
+      Serial.print ("LM");
     } else {
-      encoderLPos++;
+      //encoderLPos++;
+      Serial.print ("LP");
     }
-    Serial.print (encoderLPos);
-    
+    //Serial.print ("L: ");  
+    //Serial.print (encoderLPos);
+    //Serial.print ("\n");
   }
+  encoderLPinALast = l;
   if ((encoderRPinALast == LOW) && (r == HIGH)) {
     if (digitalRead(encoderRPinB) == LOW) {
-      encoderRPos--;
+      //encoderRPos--;
+      Serial.print ("RM"); 
     } else {
-      encoderRPos++;
+      //encoderRPos++;
+      Serial.print ("RP"); 
     }
-    Serial.print (encoderRPos);   
+    //Serial.print ("R: ");  
+    //Serial.print (encoderRPos);   
+    //Serial.print ("\n");
   }
-  
-  //Check if clicked as push button
-  lButtonState = digitalRead(encoderLClk);
-  rButtonState = digitalRead(encoderRClk);
-  if (lButtonState == HIGH)
-  {
-    Serial.print (lButtonState);
-  }else{Serial.print (0);}
-  if (rButtonState == HIGH)
-  {
-    Serial.print (rButtonState);
-  }else{Serial.print (0);}
-  encoderLPinALast = l;
   encoderRPinALast = r;
+  //Check if clicked as push button
+  lButtonState = digitalRead(encoderLSw);
+  rButtonState = digitalRead(encoderRSw);
+  if (lButtonState != lButtonState_last)
+  {
+    if (counterl % 2)
+    {
+    Serial.print ("BL");
+    //Serial.print (999);
+    //Serial.print ("\n");
+    }
+    lButtonState_last = lButtonState;
+    
+    counterl++;
+    
+  }/*else{Serial.print (0);}*/
+  if (rButtonState != rButtonState_last)
+  {
+    if (counterr % 2)
+    {
+    Serial.print ("BR");
+    //Serial.print (-999);
+    //Serial.print ("\n");
+    }
+    rButtonState_last = rButtonState;
+    counterr++;
+  }/*else{Serial.print (0);}*/
+  
 }
